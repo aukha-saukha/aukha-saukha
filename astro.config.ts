4a480ed30fef8b1084d19446bf51e4d61211b583
GK <46765920+atomic-commit@users.noreply.github.com>
2026-04-24 22:21:32 -0700

    feat: add React integration
    - install react and react-dom
    - configure @astrojs/react
    - update Astro config for React support

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
 // Copyright (c) 2026 Aukha Saukha and its affiliates.
 // SPDX-License-Identifier: MIT
 
+import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  integrations: [react()],
+
   vite: {
     resolve: {
       // Please keep these aliases in sync with tsconfig.json (compilerOptions.paths)
